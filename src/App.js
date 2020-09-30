6a154b5eec86bee7917f6f962a2c266926eed796
Najeeb Khan <khannajeeb362@gmail.com>
2020-09-30 22:00:56 +0500

    Basic App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import './App.css';
+
 import AppNavbar from './components/layouts/AppNavbar';
+import Dashboard from './components/layouts/Dashboard';
+
+import './App.css';
 
 function App() {
   return (
@@ -9,7 +12,9 @@ function App() {
       <div className="App">      
         <AppNavbar />
         <div className="container">
-          <h1> Credit Manager App </h1>
+          <Switch>
+            <Route exact path="/" component={ Dashboard } />
+          </Switch>
         </div>
       </div>
     </Router>    
